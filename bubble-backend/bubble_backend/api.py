82aa13eb049e3cc970b92ef73fb021598f1e5765
Per Carlsen <per.carlsen@oda.com>
2024-07-03 23:05:16 +0200

    Fix CORS settings

--- bubble-backend/bubble_backend/api.py
+++ bubble-backend/bubble_backend/api.py
@@ -1,7 +1,20 @@
 from bubble_backend.selectors_ import get_data_matrix
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://localhost:1234",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
