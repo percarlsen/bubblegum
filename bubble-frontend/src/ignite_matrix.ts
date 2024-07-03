067ba3d08f7be1de16391891ef23cadf8856cb0d
Per Carlsen <per.carlsen@oda.com>
2024-07-03 23:03:06 +0200

    Scrap that, going for more vanilla approach instead

---
+++ bubble-frontend/src/ignite_matrix.ts
@@ -0,0 +1,54 @@
+import Chart, { ChartItem } from 'chart.js/auto'
+
+(async function() {
+
+  const element = document.getElementById('ignite-matrix') as ChartItem | null;
+  if (!element) {
+    // TODO: indicates an error in the HTML (missing element). Deal with that.
+    return;
+  }
+  const data = await getMatrixData();
+
+  new Chart(
+    element, 
+    {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        data: [...data.map((i) => {return {x: i.x, y: i.y}})],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      animation: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+})();
+
+
+interface MatrixData {
+  x: number;
+  y: number;
+  value: number;
+}
+
+async function getMatrixData(): Promise<MatrixData[]> {
+  const res = await fetch('http://0.0.0.0:8000/data-matrix')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  const data = await res.json();
+  // Mapping to make the consumption of this function a bit nicer. Could consider moving this to the backend to avoid the added iteration here.
+  return data.map((i: any[]) => {return {'x': i[1], 'y': i[0], 'value': i[2]}});
+}
